cddabd82303e906ed44282696ec95a3f66d2f9b7
francobrida <francobrida@gmail.com>
2026-04-09 14:23:16 +0200

    Get Vibechecks TDD (GREEN)

--- app/Models/Vibecheck.php
+++ app/Models/Vibecheck.php
@@ -5,10 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory; 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 #[Fillable([ 'event_id', 'user_id','sound_score','safe_space_score','comment'])]
 
 class Vibecheck extends Model
 {
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
