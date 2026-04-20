e42e6cf946738ff69a6643eff1f39ea739529708
francobrida <francobrida@gmail.com>
2026-04-20 15:34:10 +0200

    Add Nginx configuration

--- docker/start.sh
+++ docker/start.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
-
-# Laravel cache 
-php artisan config:cache || true
-php artisan route:cache || true
-
-# Migraciones 
+php artisan config:clear
+php artisan cache:clear
 php artisan migrate --force || true
-
-# Levantar PHP-FPM en background
 php-fpm &
-
-# Levantar Nginx en foreground
 nginx -g "daemon off;"
