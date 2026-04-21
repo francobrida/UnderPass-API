11df7892cdbe45a7339ddc5b6f822cbedfb05c59
francobrida <francobrida@gmail.com>
2026-04-21 10:47:48 +0200

    refactor:  Nginx configuration and streamline start script

--- docker/start.sh
+++ docker/start.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-# 1. Preparar la app
 php artisan config:clear
 php artisan cache:clear
 php artisan migrate --force || true
 
-# 2. Asegurar permisos totales sobre la carpeta de trabajo
+
 chown -R www-data:www-data /var/www
 
-# 3. Arrancar PHP-FPM en segundo plano
 php-fpm -D
 
-# 4. Arrancar Nginx en PRIMER PLANO (esto es lo que mantiene vivo el contenedor)
+
 echo "--- LANZANDO NGINX ---"
 nginx -g "daemon off;"
