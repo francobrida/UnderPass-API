7664d8b701a186ab6d1f86c7e8e50877dd5add55
francobrida <francobrida@gmail.com>
2026-04-28 17:58:03 +0200

    added jpg flyers

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -28,7 +28,7 @@ class EventFactory extends Factory
             'is_verified' => true,
             'is_18_plus' => true,
             'stamp_token' => Str::random(32),
-            'flyer' => 'flyers/party' . rand(1, 3) . '.jpg', 
+            'flyer' => 'images/flyers/party' . rand(1, 3) . '.jpg',
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -99,7 +99,7 @@ class DatabaseSeeder extends Seeder
 
         $pastEvents = Event::where('is_verified', true)->limit(2)->get();
         foreach ($pastEvents as $event) {
-            $clubber->stampedEvents()->attach($event->id, ['stamped_at' => now()]);
+            $clubber->stampedEvents()->attach($event->id, ['scanned_at' => now()]);
         }
         
         $this->command->info('Database seeded for Underpass Barcelona.');
