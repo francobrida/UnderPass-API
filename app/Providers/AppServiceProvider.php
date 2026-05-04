ef6d6a632373617922c0be1fae3cf9e3bcde46c7
francobrida <francobrida@gmail.com>
2026-05-04 10:57:14 +0200

    HTTPS scheme in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+        
         Vouch::observe(VouchObserver::class);
     }
 }
